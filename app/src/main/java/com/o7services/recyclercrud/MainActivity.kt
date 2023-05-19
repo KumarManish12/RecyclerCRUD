92ac915cf6d6d514514625524d653bf892e22950
KumarManish12 <mkb.42700@gmail.com>
2023-05-19 15:13:15 +0530

    Search Bar Added
    Search Bar Added

--- app/src/main/java/com/o7services/recyclercrud/MainActivity.kt
+++ app/src/main/java/com/o7services/recyclercrud/MainActivity.kt
@@ -4,11 +4,14 @@ import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.DocumentChange.Type.*
+import com.google.firebase.firestore.auth.User
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
@@ -22,10 +25,11 @@ class MainActivity : AppCompatActivity(), ClickList {
     val db = Firebase.firestore
     private lateinit var auth: FirebaseAuth
     var userList=ArrayList<UserModel>()
+    var shownUserModel = ArrayList<UserModel>()
     lateinit var adapter: UserAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         auth = Firebase.auth
         binding.btnsignout.setOnClickListener {
@@ -35,39 +39,43 @@ class MainActivity : AppCompatActivity(), ClickList {
 
         }
 
-       /* db.collection("User").get().addOnSuccessListener {
+         /*db.collection("User").get().addOnSuccessListener {
             for (snapshot in it){
                 val userModel = snapshot.toObject(UserModel::class.java)
                 userList.add(userModel)
-
             }
         }*/
         db.collection("User").addSnapshotListener { value, error ->
-            for (snapshots in value!!.documentChanges){
-                when(snapshots.type){
-                    ADDED ->{
-                        var userModel=UserModel()
-                        userModel=snapshots.document.toObject(userModel::class.java)
-                        userModel.key=snapshots.document.id?:""
+//            if(error.toString().isNotEmpty()){
+//                return@addSnapshotListener
+//            }
+           for (snapshots in value?.documentChanges!!) {
+                when (snapshots.type) {
+                    ADDED -> {
+                        var userModel = UserModel()
+                        userModel = snapshots.document.toObject(userModel::class.java)
+                        userModel.key = snapshots.document.id ?: ""
                         userList.add(userModel)
+                        shownUserModel.add(userModel)
+
                     }
-                    MODIFIED ->{
-                        var userModel=UserModel()
-                        userModel=snapshots.document.toObject(userModel::class.java)
-                        userModel.key=snapshots.document.id?:""
-                        for (i in 0..userList.size-1){
-                            if ((snapshots.document.id?:"").equals(userList[i].key)){
-                                userList.set(i,userModel)
+                    MODIFIED -> {
+                        var userModel = UserModel()
+                        userModel = snapshots.document.toObject(userModel::class.java)
+                        userModel.key = snapshots.document.id ?: ""
+                        for (i in 0..userList.size - 1) {
+                            if ((snapshots.document.id ?: "").equals(userList[i].key)) {
+                                userList.set(i, userModel)
                                 break
                             }
                         }
                     }
                     REMOVED -> {
-                        var userModel=UserModel()
-                        userModel=snapshots.document.toObject(userModel::class.java)
-                        userModel.key=snapshots.document.id?:""
-                        for(i in 0..userList.size-1){
-                            if ((snapshots.document.id?:"").equals(userList[i].key)){
+                        var userModel = UserModel()
+                        userModel = snapshots.document.toObject(userModel::class.java)
+                        userModel.key = snapshots.document.id ?: ""
+                        for (i in 0..userList.size - 1) {
+                            if ((snapshots.document.id ?: "").equals(userList[i].key)) {
                                 userList.removeAt(i)
                                 break
                             }
@@ -76,86 +84,95 @@ class MainActivity : AppCompatActivity(), ClickList {
                 }
             }
         }
-        adapter= UserAdapter(userList,this)
-        binding.recycleview.adapter=adapter
-        binding.recycleview.layoutManager=LinearLayoutManager(this)
-        binding.btnok.setOnClickListener {
-            val dialogBinding=AddLayoutBinding.inflate(layoutInflater)
-            val customDialog=Dialog(this)
-            customDialog.setContentView(dialogBinding.root)
+        adapter = UserAdapter(shownUserModel, this)
+        binding.recycleview.adapter = adapter
+        binding.recycleview.layoutManager = LinearLayoutManager(this)
+        binding.etsearch.doOnTextChanged { text, start, before, count ->
+            if ((text ?: "").isEmpty()) {
+                shownUserModel.addAll(userList)
+            }else if ((text?:"").length >2){
+                shownUserModel.clear()
+                var list= java.util.ArrayList<UserModel>()
+                list.addAll(userList.filter { element -> element.name?.contains((text?:""),ignoreCase = true)==true })
+                shownUserModel.addAll(list)
+           }
+            }
+            binding.btnok.setOnClickListener {
+                val dialogBinding = AddLayoutBinding.inflate(layoutInflater)
+                val customDialog = Dialog(this)
+                customDialog.setContentView(dialogBinding.root)
 
-            dialogBinding.btnAdd.setOnClickListener {
-                if (dialogBinding.etName.text.toString().isEmpty()){
-                    dialogBinding.etName.error="enter name"
+                dialogBinding.btnAdd.setOnClickListener {
+                    if (dialogBinding.etName.text.toString().isEmpty()) {
+                        dialogBinding.etName.error = "enter name"
+                    } else if (dialogBinding.etRollNo.text.toString().isEmpty()) {
+                        dialogBinding.etRollNo.error = "enter rollno"
+                    } else {
+                        val userModel = UserModel(dialogBinding.etName.text.toString(),
+                            dialogBinding.etRollNo.text.toString())
+                        db.collection("User")
+                            .add(userModel)
+                            .addOnSuccessListener {
+                             customDialog.dismiss()
+                            }
+                            .addOnFailureListener {
+                                System.out.println("in error ${it.message.toString()}")
+                                Toast.makeText(this,"Data does not added ${it.message.toString()}" ,Toast.LENGTH_LONG).show()
+                            }
+
+
+
+                        adapter.notifyDataSetChanged()
+                    }
                 }
-                else if (dialogBinding.etRollNo.text.toString().isEmpty()){
-                    dialogBinding.etRollNo.error="enter rollno"
-                }
-                else{
-                   val userModel=UserModel(dialogBinding.etName.text.toString(),dialogBinding.etRollNo.text.toString())
+                customDialog.show()
+            }
+        }
+
+        override fun listClicked(position: Int) {
+            val dialogBinding1 = EditLayoutBinding.inflate(layoutInflater)
+            val customDialog1 = Dialog(this)
+            customDialog1.setContentView(dialogBinding1.root)
+            dialogBinding1.etName.setText(userList[position].name)
+            dialogBinding1.etRollNo.setText(userList[position].mobile)
+            dialogBinding1.btnEdit.setOnClickListener {
+                if (dialogBinding1.etName.text.toString().isEmpty()) {
+                    dialogBinding1.etName.error = "enter name"
+                } else if (dialogBinding1.etRollNo.text.toString().isEmpty()) {
+                    dialogBinding1.etRollNo.error = "enter rollno"
+                } else {
+                    var updatUserModel = UserModel()
+                    updatUserModel.name = dialogBinding1.etName.text.toString()
+                    updatUserModel.mobile = dialogBinding1.etRollNo.text.toString()
+                    updatUserModel.key = userList[position].key
                     db.collection("User")
-                        .add(userModel)
-                        .addOnSuccessListener {
-
+                        .document(userList[position].key ?: "")
+                        .set(updatUserModel).addOnSuccessListener {
                         }
-
-
-
-                    customDialog.dismiss()
+                    //  userList.set(position,UserModel(dialogBinding1.etName.text.toString(),dialogBinding1.etRollNo.text.toString()))
                     adapter.notifyDataSetChanged()
+                    customDialog1.dismiss()
                 }
             }
-            customDialog.show()
-        }
-    }
+            dialogBinding1.btnDelete.setOnClickListener {
+                if (dialogBinding1.etName.text.toString().isEmpty()) {
+                    dialogBinding1.etName.error = "enter name"
+                } else if (dialogBinding1.etRollNo.text.toString().isEmpty()) {
+                    dialogBinding1.etRollNo.error = "enter rollno"
+                } else {
+                    var deleteUserModel = UserModel()
+                    deleteUserModel.name = dialogBinding1.etName.text.toString()
+                    deleteUserModel.mobile = dialogBinding1.etRollNo.text.toString()
+                    deleteUserModel.key = userList[position].key
+                    db.collection("User")
+                        .document(deleteUserModel.key ?: "").delete()
+                    // userList.remove(UserModel(dialogBinding1.etName.text.toString(),dialogBinding1.etRollNo.text.toString()))
+                    adapter.notifyDataSetChanged()
+                    customDialog1.dismiss()
+                }
+            }
+            customDialog1.show()
 
-    override fun listClicked(position: Int) {
-        val dialogBinding1=EditLayoutBinding.inflate(layoutInflater)
-        val customDialog1=Dialog(this)
-        customDialog1.setContentView(dialogBinding1.root)
-        dialogBinding1.etName.setText(userList[position].name)
-        dialogBinding1.etRollNo.setText(userList[position].mobile)
-        dialogBinding1.btnEdit.setOnClickListener {
-            if (dialogBinding1.etName.text.toString().isEmpty()){
-                dialogBinding1.etName.error="enter name"
-            }
-            else if (dialogBinding1.etRollNo.text.toString().isEmpty()){
-                dialogBinding1.etRollNo.error="enter rollno"
-            }
-            else{
-                var updatUserModel=UserModel()
-                updatUserModel.name=dialogBinding1.etName.text.toString()
-                updatUserModel.mobile=dialogBinding1.etRollNo.text.toString()
-                updatUserModel.key=userList[position].key
-                db.collection("User")
-                    .document(userList[position].key?:"")
-                    .set(updatUserModel).addOnSuccessListener {
-                        }
-              //  userList.set(position,UserModel(dialogBinding1.etName.text.toString(),dialogBinding1.etRollNo.text.toString()))
-                adapter.notifyDataSetChanged()
-                customDialog1.dismiss()
-            }
         }
-        dialogBinding1.btnDelete.setOnClickListener {
-            if (dialogBinding1.etName.text.toString().isEmpty()){
-                dialogBinding1.etName.error="enter name"
-            }
-            else if (dialogBinding1.etRollNo.text.toString().isEmpty()){
-                dialogBinding1.etRollNo.error="enter rollno"
-            }
-            else{
-                var deleteUserModel=UserModel()
-                deleteUserModel.name=dialogBinding1.etName.text.toString()
-                deleteUserModel.mobile=dialogBinding1.etRollNo.text.toString()
-                deleteUserModel.key=userList[position].key
-                db.collection("User")
-                    .document(deleteUserModel.key?:"").delete()
-               // userList.remove(UserModel(dialogBinding1.etName.text.toString(),dialogBinding1.etRollNo.text.toString()))
-                adapter.notifyDataSetChanged()
-                customDialog1.dismiss()
-            }
-        }
-       customDialog1.show()
 
-    }
 }
